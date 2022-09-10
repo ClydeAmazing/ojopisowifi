560251c533f9946b60ce13a7ea6c8c8ff375bd7e
Clyde <alinoclyde.juan24@gmail.com>
2022-09-10 11:12:59 +0800

    Added referrer mac address in case client authenticated from other browser then openned another browser when checking time

--- opw/views.py
+++ opw/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import redirect
 from django.views import View
+from app.models import Clients
 
 
 class Main(View):
     def get(self, request):
         fas = request.GET.get('fas', None)
+        referrer_mac = request.GET.get('referrer', None)
 
         if fas:
             request.session['fas'] = fas
+
+        if referrer_mac:
+            client = Clients.objects.get(MAC_Address=referrer_mac)
+            request.session['fas'] = client.FAS_Session
+
         return redirect('app:portal')
