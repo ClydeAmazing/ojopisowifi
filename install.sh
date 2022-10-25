816f1d6c116fe9641ecb98a260ad1c97f88e3fb1
John Clyde Gonzaga <alinoclyde.juan24@gmail.com>
2022-10-25 14:00:16 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,21 +12,18 @@ echo ''
 echo 'Installing dependencies'
 apt-get install build-essential libssl-dev libffi-dev python3-dev python3-venv python3-pip redis-server nginx dnsmasq gunicorn git systemd -y
 echo ''
-echo 'Changing directory to sudoadmin'
-cd /home/sudoadmin
-echo ''
 echo 'Creating src directory'
-mkdir src && cd src
+mkdir /home/sudoadmin/src
 echo ''
 echo 'Creating python virtual environment'
-python3 -m venv venv
+python3 -m venv /home/sudoadmin/src/venv
 echo ''
 echo 'Downloading source code from github'
 export GIT_SSL_NO_VERIFY=1
 git clone https://github.com/ClydeAmazing/ojopisowifi.git
 echo ''
 echo 'Activating virtual environment'
-source venv/bin/activate
+source /home/sudoadmin/src/venv/bin/activate
 echo ''
 echo 'Upgrading python pip, setuptools and wheel'
 pip install --upgrade pip setuptools wheel --trusted-host pypi.org --trusted-host files.pythonhosted.org
@@ -37,11 +34,33 @@ echo ''
 echo 'Setting file permissions'
 chown sudoadmin:root /home/sudoadmin/src/ojopisowifi/ && chown sudoadmin:root /home/sudoadmin/src/ojopisowifi/db.sqlite3 && chmod +x /home/sudoadmin/src/ojopisowifi/hooks.py
 echo ''
+echo 'Removing default nginx profile'
+rm /etc/nginx/sites-enabled/default
+echo ''
 echo 'Copying system files to target locations'
 cp /home/sudoadmin/src/ojopisowifi/files/gunicorn.service /etc/systemd/system/gunicorn.service
 cp /home/sudoadmin/src/ojopisowifi/files/hooks.service /etc/systemd/system/hooks.service
 cp /home/sudoadmin/src/ojopisowifi/files/opw_init.service /etc/systemd/system/opw_init.service
+cp /home/sudoadmin/src/ojopisowifi/files/sweep.service /etc/systemd/system/sweep.service
 cp /home/sudoadmin/src/ojopisowifi/files/opw.conf /etc/nginx/conf.d/
+echo ''
+echo 'Reloadin daemon files'
+systemctl daemon-reload
+echo ''
+echo 'Enabling gunicorn service'
+systemctl enable gunicorn.service
+echo ''
+echo 'Enabling hooks service'
+systemctl enable hooks.service
+echo ''
+echo 'Enabling opw_init service'
+systemctl enable opw_init.service
+echo ''
+echo 'Starting services'
+systemctl start gunicorn.service
+systemctl start hooks.service
+systemctl start opw_init.service
+systemctl start sweep.service
 echo ''
 echo 'Performing collecstatic command'
 python /home/sudoadmin/src/ojopisowifi/manage.py collectstatic
@@ -49,13 +68,15 @@ echo ''
 echo 'Deactivating Python Virtual Environment'
 deactivate
 echo ''
-echo 'Backing up dnsmasq.conf'
-mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
-echo ''
-echo 'Copying new dnsmasq config'
-cp /home/sudoadmin/src/ojopisowifi/files/dnsmasq.conf /etc/dnsmasq.conf
-echo ''
-echo 'Backing up network interface config'
-mv /etc/network/interfaces /etc/network/interfaces.orig
-echo 'Copying new network interface config'
-cp /home/sudoadmin/src/ojopisowifi/files/interfaces /etc/network/interfaces
+echo 'Restarting nginx server'
+systemctl restart nginx.service
+# echo 'Backing up dnsmasq.conf'
+# mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
+# echo ''
+# echo 'Copying new dnsmasq config'
+# cp /home/sudoadmin/src/ojopisowifi/files/dnsmasq.conf /etc/dnsmasq.conf
+# echo ''
+# echo 'Backing up network interface config'
+# mv /etc/network/interfaces /etc/network/interfaces.orig
+# echo 'Copying new network interface config'
+# cp /home/sudoadmin/src/ojopisowifi/files/interfaces /etc/network/interfaces
