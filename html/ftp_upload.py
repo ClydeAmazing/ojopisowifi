d05ba4ae5d73ef7edfadea9dad07025a6682cd12
Clyde <alinoclyde.juan24@gmail.com>
2023-09-16 11:27:14 +0300

    Minor updates and added development helper functions

---
+++ html/ftp_upload.py
@@ -0,0 +1,42 @@
+import os
+import paramiko
+
+def sftp_upload(sftp, local_path, remote_path):
+    try:
+        sftp.put(local_path, remote_path)
+        print("File upload successful.")
+    except Exception as e:
+        print("An error occurred:", e)
+
+files = [
+    {
+        'file': './main.css',
+        'target': '/etc/opennds/htdocs/'
+    },
+    {
+        'file': './splash.html',
+        'target': '/etc/opennds/htdocs/'
+    },
+    {
+        'file': './theme_click-to-continue-legacy.sh',
+        'target': '/usr/lib/opennds/'
+    }
+]
+
+hostname = '192.168.8.1'
+port = 22
+username = 'root'
+password = '<Jeddah123/>'
+
+transport = paramiko.Transport((hostname, port))
+transport.connect(username=username, password=password)
+
+sftp = paramiko.SFTPClient.from_transport(transport)
+
+for local_file in files:
+    local_path = local_file['file']
+    remote_path = os.path.join(local_file['target'], os.path.basename(local_path))
+    sftp_upload(sftp, local_path, remote_path)
+
+sftp.close()
+transport.close()
